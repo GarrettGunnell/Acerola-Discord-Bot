5fe695eb9d4c7a95c5d12a1d207e9783a1d58819
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-08-30 18:12:46 -0700

    Bot keeps track of times 'bro' is said in chat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 import random
+import json
+from collections import defaultdict
 
 acerola_responses = ['nice', 'pog', 'damn that sucks', 'you should get some bitches', 'hell yeah', 'bro', 'bro...']
 help_message = "\
@@ -11,6 +13,10 @@ help_message = "\
 \tSimulate a conversation with Acerola himself.\n\
 ```\
 "
+
+def def_value():
+    return 0
+bro_leaderboard = defaultdict(def_value)
 
 TOKEN = open("token.txt","r").readline()
 
@@ -25,16 +31,28 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    content = message.content.lower()
+    user = str(message.author).split('#')[0]
+    
     if message.author == client.user:
         return
 
-    if message.content == '!help':
+    if content == '!help':
         await message.channel.send(help_message)
         return
 
-    if message.content == '!respond':
+    if content == '!respond':
         await message.channel.send(random.choice(acerola_responses))
         return
+
+    if content == '!broboard':
+        leaderboard = json.dumps(bro_leaderboard)[1:-1]
+
+        await message.channel.send("```\n" + leaderboard + "\n```")
+        return
+
+    if 'bro' in content or 'vro' in content or 'bri' in content:
+        bro_leaderboard[user] += 1
 
     
 client.run(TOKEN)
